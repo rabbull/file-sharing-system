44167f3c155d2be8ff4cc9acebd41560c28ca36c
Karl Liu <karl_liu@outlook.com>
2019-11-24 19:50:19 +0800

    wtf

--- fssctl.py
+++ fssctl.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import json
 import argparse as ap
+
+from daemon_config import DaemonConfig
 
 def fssctl_main():
     command_parser = ap.ArgumentParser()
@@ -9,7 +12,6 @@ def fssctl_main():
     group.add_argument('-s', '--search', action='store_true')
 
     args = command_parser.parse_args()
-
     if len(sys.argv) > 1:
         sys.argv[0] = ' '.join(sys.argv[:2])
         sys.argv.remove(sys.argv[1])
@@ -17,11 +19,29 @@ def fssctl_main():
         config_main()
     elif args.search:
         search_main()
-    elif args.daemon:
-        daemon.main()
     else:
         command_parser.print_usage()
-        exit(1)
+        exit(-1)
+
+
+def config_main():
+    raise NotImplementedError()
+
+def search_main():
+    argparser = ap.ArgumentParser()
+    argparser.add_argument('-c', '--config', metavar='PATH')
+    argparser.add_argument('--neighbor-list', metavar='PATH')
+    argparser.add_argument('-n', '--name', metavar='FILENAME')
+
+    args = argparser.parse_args()
+    if len(sys.argv) > 1:
+        sys.argv[0] = ' '.join(sys.argv[1])
+        sys.argv.remove(sys.argv[1])
+    
+    cfg = DaemonConfig()
+    if args.config:
+        with open(args.config, 'r') as fp:
+            cfg.__dict__ = json.load(fp)
 
 
 if __name__ == '__main__':
